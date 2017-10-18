9703b65a8554663c1a77b6901ed8b6d456134a40
clairepds <poildessous.claire@gmail.com>
2017-10-18 17:50:31 +0200

    jour 3
    création js

---
+++ javascript.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function(){
+
+
+$("h1").click(function(){
+
+  $("header").toggle(1000);
+
+});
+$.getJSON("https://api.airtable.com/v0/appBregcrPIWd0vaK/musics?api_key=keyCpj3STqVxqRU46",
+					function(data) {
+          console.log(data);
+          $.each(data.records, function(key,val) {
+            console.log(val.fields.title);
+            $('<li>'+val.fields.title+'</li>').appendTo('#musiques');
+            $("<iframe src='https://open.spotify.com/embed?uri="+val.fields['spotify-link']+"&view=coverart frameborder='0' allowtransparency='true'></iframe>").appendTo('#musiques');
+          });
+					}
+);
+});
