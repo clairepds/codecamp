70e9eea48ea2ca8644a99fdeb1b4dbd07a2635c5
clairepds <poildessous.claire@gmail.com>
2017-10-19 17:27:30 +0200

    jour 4
    optimisation du site

--- javascript.js
+++ javascript.js
@@ -12,9 +12,33 @@ $.getJSON("https://api.airtable.com/v0/appBregcrPIWd0vaK/musics?api_key=keyCpj3S
           console.log(data);
           $.each(data.records, function(key,val) {
             console.log(val.fields.title);
-            $('<li>'+val.fields.title+'</li>').appendTo('#musiques');
-            $("<iframe src='https://open.spotify.com/embed?uri="+val.fields['spotify-link']+"&view=coverart frameborder='0' allowtransparency='true'></iframe>").appendTo('#musiques');
+
+            $("<iframe src='https://open.spotify.com/embed?uri="+val.fields['spotify-link']+"' frameborder='0' width='80' height='180' allowtransparency='true'></iframe>").appendTo('#mus-autres');
+
           });
 					}
 );
+});
+$(document).ready(function(){
+
+  $(".button").click(function(){
+      $("header").toggle(1000);
+  });
+
+  $.getJSON("https://api.airtable.com/v0/appy7PzQ2wWqQWwY6/musics?api_key=keyC15K3Qkejq0vGE",
+
+  function(data) {
+      console.log(data);
+      $.each(data.records, function(key,val) {
+          console.log(val.fields.title);
+
+          $('<li>'+val.fields.title+'</li>').appendTo('#musiques');
+
+
+      });
+
+    }
+  );
+
+
 });
